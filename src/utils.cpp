ce904e30532290fc1f5dfff1f1f20a491b753d33
daguai <13935474921@163.com>
2025-08-21 17:36:19 +0800

    修改utils工具类

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,7 +6,15 @@
 
 namespace coro::utils 
 {
-    
+auto set_fd_noblock(int fd) noexcept -> void
+{
+    int flags = fcntl(fd,F_GETFL,0);
+    assert(flags >= 0);
+
+    flags |= O_NONBLOCK;
+    assert(fcntl(fd, F_SETFL,flags) >= 0);
+}
+
 
 auto get_null_fd() noexcept -> int
 {
